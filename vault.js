68200bf328a6ee6f81d6d893e66cb916a24e1a45
Seung-Woo Seo <seungwoo103@gmail.com>
2024-09-04 21:23:59 -0400

    created alert box for html in js

---
+++ vault.js
@@ -0,0 +1,5 @@
+const x = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+
+
