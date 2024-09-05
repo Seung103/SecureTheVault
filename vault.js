596306dca6f2e78f39b9d5f075c4352afd2c81a3
Seung-Woo Seo <seungwoo103@gmail.com>
2024-09-04 21:28:33 -0400

    created variables, trying to create console.log to add up to those numbers

--- vault.js
+++ vault.js
@@ -1,5 +1,11 @@
-const x = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
-
 alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+
+const x = 10
+const y = 40
+const z = 39
+
+console.log(x);
+console.log(y);
+console.log(z);
 
 
