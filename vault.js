3cc5c877089311acdc3852652d7d3f1cbc9a5e40
Seung-Woo Seo <seungwoo103@gmail.com>
2024-09-04 22:13:23 -0400

    created some more notes on js

--- vault.js
+++ vault.js
@@ -1,11 +1,20 @@
 alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
 
-const x = 10
-const y = 40
-const z = 39
+//added text to be clear on the vault combination
+// 10 - 40 - 39
+const x = 15 - 5;
+const y = 2 * 20;
+const z = 78 / 2;
+
+const message = x - z - y
+
+alert(message);
+
+
 
 console.log(x);
+// expected output: 10 on html console
 console.log(y);
+// expected output: 40 on html console
 console.log(z);
-
-
+// expected output: 39 on html console
